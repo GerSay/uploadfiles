89a760041568429c89475091eeb67ea21d67131b
GerSay <kryt.master@yandex.ru>
2024-01-17 10:09:04 +0300

    first commit

---
+++ src/test/java/gersay/uploadfiles/UploadfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package gersay.uploadfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
